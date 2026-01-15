46ff65c8bd312508a0d7419e6c378ba226bb5378
CNFeffery <fefferypzy@gmail.com>
2026-01-15 10:36:42 +0800

    fix: lint

--- examples/react-bits-grid-scan-demo/app.py
+++ examples/react-bits-grid-scan-demo/app.py
@@ -5,14 +5,14 @@ from dash_vite_plugin import VitePlugin, NpmPackage
 from dash.dependencies import Input, State, ClientsideFunction
 
 vite_plugin = VitePlugin(
-    build_assets_paths=["assets"],
-    entry_js_paths=["assets/callback.js"],
+    build_assets_paths=['assets'],
+    entry_js_paths=['assets/callback.js'],
     npm_packages=[
-        NpmPackage("react"),
-        NpmPackage("react-dom"),
-        NpmPackage("three"),
-        NpmPackage("postprocessing"),
-        NpmPackage("face-api.js"),
+        NpmPackage('react'),
+        NpmPackage('react-dom'),
+        NpmPackage('three'),
+        NpmPackage('postprocessing'),
+        NpmPackage('face-api.js'),
     ],
     download_node=True,
     clean_after=False,  # 开发阶段建议设置为False以加速构建
@@ -26,16 +26,16 @@ vite_plugin.use(app)
 
 app.layout = html.Div(
     [
-        fuc.FefferyTimeout(id="trigger-background-init", delay=100),
-        html.Div(id="background-mount"),
+        fuc.FefferyTimeout(id='trigger-background-init', delay=100),
+        html.Div(id='background-mount'),
     ]
 )
 
 app.clientside_callback(
-    ClientsideFunction(namespace="clientside", function_name="renderBackground"),
-    Input("trigger-background-init", "timeoutCount"),
-    State("background-mount", "id"),
+    ClientsideFunction(namespace='clientside', function_name='renderBackground'),
+    Input('trigger-background-init', 'timeoutCount'),
+    State('background-mount', 'id'),
 )
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
--- examples/react-bits-shiny-text-demo/app.py
+++ examples/react-bits-shiny-text-demo/app.py
@@ -8,12 +8,12 @@ from dash.dependencies import Input, State, ClientsideFunction
 
 
 vite_plugin = VitePlugin(
-    build_assets_paths=["assets"],
-    entry_js_paths=["assets/callback.js"],
+    build_assets_paths=['assets'],
+    entry_js_paths=['assets/callback.js'],
     npm_packages=[
-        NpmPackage("react"),
-        NpmPackage("react-dom"),
-        NpmPackage("motion"),
+        NpmPackage('react'),
+        NpmPackage('react-dom'),
+        NpmPackage('motion'),
     ],
     download_node=True,
     clean_after=False,  # 开发阶段建议设置为False以加速构建
@@ -27,16 +27,16 @@ vite_plugin.use(app)
 
 app.layout = html.Div(
     [
-        fuc.FefferyTimeout(id="trigger-text-init", delay=100),
+        fuc.FefferyTimeout(id='trigger-text-init', delay=100),
         fac.AntdCenter(
-            id="text-mount",
+            id='text-mount',
             style=style(
-                position="fixed",
-                width="100vw",
-                height="100vh",
+                position='fixed',
+                width='100vw',
+                height='100vh',
                 left=0,
                 top=0,
-                background="#060010",
+                background='#060010',
                 fontSize=56,
             ),
         ),
@@ -44,10 +44,10 @@ app.layout = html.Div(
 )
 
 app.clientside_callback(
-    ClientsideFunction(namespace="clientside", function_name="renderText"),
-    Input("trigger-text-init", "timeoutCount"),
-    State("text-mount", "id"),
+    ClientsideFunction(namespace='clientside', function_name='renderText'),
+    Input('trigger-text-init', 'timeoutCount'),
+    State('text-mount', 'id'),
 )
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
